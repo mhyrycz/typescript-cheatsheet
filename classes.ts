6afe65056a63882f3572cd11165252416320d312
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-07-28 19:15:19 +0200

    Generics example added.

--- classes.ts
+++ classes.ts
@@ -1,15 +1,15 @@
 class Vehicle {
 	wheels: number;
-    type: string;
-    color: string;
+	type: string;
+	color: string;
 
-    //two ways of passing values to initialize and declare variables in typescript
-    constructor(color: string, public broken: boolean){
-        this.color = color
-    }
+	//two ways of passing values to initialize and declare variables in typescript
+	constructor(color: string, public broken: boolean) {
+		this.color = color;
+	}
 
 	summary(): void {
-		console.log(`My ${this.type} has ${this.wheels} wheels and it is ${this.broken ? "" : "not"} broken.`);
+		console.log(`My ${this.type} has ${this.wheels} wheels and it is ${this.broken ? '' : 'not'} broken.`);
 	}
 }
 
@@ -18,27 +18,41 @@ class Car extends Vehicle {
 		super(color, broken);
 		this.wheels = 4;
 		this.type = 'Car';
-    }
-    
-    isItGrey():boolean {
-        return this.color === "grey"
-    }
+	}
+
+	isItGrey(): boolean {
+		return this.color === 'grey';
+	}
 }
 
 class Bicycle extends Vehicle {
 	constructor(color: string, broken: boolean, wheels: number) {
 		super(color, broken);
-		this.wheels = wheels
+		this.wheels = wheels;
 		this.type = 'Bicycle';
 	}
 }
 
-const myBike = new Bicycle("green", true, 2);
+const myBike = new Bicycle('green', true, 2);
 
-const myCar = new Car("grey", false);
+const myCar = new Car('grey', false);
 
 myBike.summary();
 
 myCar.summary();
 
 console.log(myCar.isItGrey());
+
+// Generics
+
+class HoldAnything<TypeOfData> {
+	data: TypeOfData;
+}
+
+const string = new HoldAnything<string>();
+
+string.data = 'string';
+
+const number = new HoldAnything<number>();
+
+number.data = 123;
